ca5480b33074f0f81f6f8b26d81993d6607663e8
Rongsong Shen <rongsong.shen@gmail.com>
2014-01-22 16:59:03 +0800

    add init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,4 @@
 ;;; Add initialization here
+(add-to-list 'load-path "/home/rshen/.emacs.d/")
+(add-to-list 'load-path "/home/rshen/.emacs.d/org/lisp")
+(add-to-list 'load-path "/home/rshen/.emacs.d/org/contrib/lisp")
