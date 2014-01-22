3acfe9734850a9920484eacfc07bb2ecfb9b436a
Rongsong Shen <rongsong.shen@gmail.com>
2014-01-22 17:27:15 +0800

    add init

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,12 @@
 ;;; Add initialization here
-(add-to-list 'load-path "/home/rshen/.emacs.d/")
-(add-to-list 'load-path "/home/rshen/.emacs.d/org/lisp")
-(add-to-list 'load-path "/home/rshen/.emacs.d/org/contrib/lisp")
+(defvar *custom-dir* "/home/rshen/.emacs.d")
+(add-to-list 'load-path *custom-dir*)
+
+;; define the package which are needed
+;; to be loaded
+(defvar *load-init* '("org.el"))
+(defvar *misc-dir* (concat *custom-dir* "/misc"))
+
+;; load initialize packages
+(dolist (pkg *load-init*)
+  (load-file pkg))
