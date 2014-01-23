ed4b0366be46cc433d81ce5020377740b6d702cb
Rongsong Shen <rongsong.shen@gmail.com>
2014-01-23 12:00:22 +0800

    add c, org mode

---
+++ .emacs.d/my-c.el
@@ -0,0 +1,69 @@
+;; offset customizations not in my-c-style
+;; This will take precedence over any setting of the syntactic symbol
+;; made by a style.
+
+(defun my-c-initialization-hook ()
+  (progn (define-key c-mode-base-map "\C-m" 'c-context-line-break)))
+
+(add-hook 'c-initialization-hook 'my-c-initialization-hook)
+
+(setq c-offsets-alist '((member-init-intro . ++)))
+
+;; Create my personal style.
+
+(defconst my-c-style
+  '((c-tab-always-indent        . t)
+					;    (c-comment-only-line-offset . 4)
+    (c-hanging-braces-alist     . ((substatement-open after)
+                                   (brace-list-open)))
+    (c-hanging-colons-alist     . ((member-init-intro before)
+                                   (inher-intro)
+                                   (case-label after)
+                                   (label after)
+                                   (access-label after)))
+    (c-cleanup-list             . (scope-operator
+                                   empty-defun-braces
+                                   defun-close-semi))
+    (c-offsets-alist            . ((arglist-close . c-lineup-arglist)
+                                   (substatement-open . 0)
+                                   (case-label        . 4)
+                                   (block-open        . 0)
+                                   (knr-argdecl-intro . -)))
+    (c-echo-syntactic-information-p . t))
+  "My C Programming Style")
+
+(c-add-style "PERSONAL" my-c-style)
+
+;; Customizations for all modes in CC Mode.
+(defun my-c-mode-common-hook ()
+  ;; set my personal style for the current buffer
+  (c-set-style "PERSONAL")
+  ;; other customizations
+  (setq tab-width 4
+        ;; this will make sure spaces are used instead of tabs
+        indent-tabs-mode nil)
+  ;; we like auto-newline, but not hungry-delete
+  (c-toggle-auto-newline 1)
+  (gtags-mode 1)
+  (hs-minor-mode 1))
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+(defun add-c-function-header (function-name)
+  "Add header description of c/c++ functions"
+  (interactive "sFunction Name:")
+  (progn
+    (insert "/*\n")
+    (insert " *---------------------------------------------------------------------------------\n")
+    (insert " *\n")
+    (insert (concat " *  " function-name "() --\n"))
+    (insert " *\n")
+    (insert " *   DESCRIPTION:\n")
+    (insert " *\n")
+    (insert " *   ARGS:\n")
+    (insert " *\n")
+    (insert " *   RETURN:\n")
+    (insert " *\n")
+    (insert " *---------------------------------------------------------------------------------\n")
+    (insert " */\n")
+    ))
--- .emacs.d/org.el
+++ .emacs.d/org.el
@@ -30,5 +30,58 @@
  '(org-eukleides-path "/usr/bin/eukleides")
  `(org-ditaa-jar-path ,(concat *org-root* "contrib/scripts/ditaa.jar"))
  '(org-enforce-todo-dependencies t)
+ '(org-export-backends (quote (ascii html icalendar latex odt)))
+ '(org-latex-default-packages-alist 
+   (quote (("T1" "fontenc" t) 
+	   ("" "fixltx2e" nil) 
+	   ("" "graphicx" t) 
+	   ("" "longtable" nil) 
+	   ("" "float" nil) 
+	   ("" "wrapfig" nil) 
+	   ("" "soul" t) 
+	   ("" "textcomp" t) 
+	   ("" "marvosym" t) 
+	   ("" "wasysym" t) 
+	   ("" "latexsym" t) 
+	   ("" "amssymb" t) 
+	   ("" "amstext" nil) 
+	   ("" "hyperref" nil) "\\tolerance=1000")))
+
  `(org-plantuml-jar-path ,(concat *misc-dir* "/plantuml.jar")))
 
+(defun org-use-xelatex ()
+  (interactive)
+  (progn (setq org-latex-pdf-process
+               (quote ("xelatex -interaction nonstopmode -output-directory %o %f"
+                       "bibtex %b" "xelatex -interaction nonstopmode -output-directory %o %f"
+                       "xelatex -interaction nonstopmode -output-directory %o %f")))
+         t))
+
+(defun org-ignore-title-toc ()
+  (interactive)
+  (progn (setq org-latex-title-command "\\relax")
+         (setq org-latex-toc-command "\\relax")))
+
+(defun org-disable-title-and-toc ()
+  (interactive)
+  (setq org-latex-title-command "\\relax")
+  (setq org-latex-toc-command "\\relax"))
+
+(setq org-babel-temporary-directory "/tmp/babel/")
+
+(defun org-odt-publish-to-odt (plist filename pub-dir)
+  (message "odt publishing\n")
+  (org-publish-org-to 'odt filename
+                      (concat "." (or (plist-get plist :odt-extension)
+                                      org-html-extension "odt"))
+                      plist pub-dir))
+
+(defun org-before-publish ()
+  (if (boundp 'org-babel-temporary-directory)
+      (unless (file-exists-p org-babel-temporary-directory)
+        (progn
+          (make-directory org-babel-temporary-directory t)))))
+
+;; Using xelatex to enable
+;; CJK support
+(add-hook 'org-mode-hook 'org-use-xelatex)
